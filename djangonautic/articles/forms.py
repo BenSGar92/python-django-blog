107b6275f9fe4df787fa986e2ef0e0bc7ea694bd
Ben G <bensgar92@gmail.com>
2021-02-10 11:54:53 -0700

    updated slug field to be disabled

--- djangonautic/articles/forms.py
+++ djangonautic/articles/forms.py
@@ -5,3 +5,9 @@ class CreateArticle(forms.ModelForm):
     class Meta:
         model = models.Article
         fields = ['title', 'body', 'slug', 'thumb']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["slug"].disabled = True
+        # Or to set READONLY
+        self.fields["slug"].widget.attrs["readonly"] = True
